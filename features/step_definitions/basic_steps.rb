16063c8d11a98f65b8b6fd40153006672d19240b
oliverochman <oliver.ochman@hotmail.com>
2018-10-03 10:19:04 +0200

    add feature test and step defs

---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,16 @@
+Given("I visit the site") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be redirected to index page") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
