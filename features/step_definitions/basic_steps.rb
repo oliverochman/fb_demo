4bbad201b4d5299c37262bfd5da982e6da1dd5ba
oliverochman <oliver.ochman@hotmail.com>
2018-10-03 10:22:05 +0200

    add link to login with fb

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,9 +1,9 @@
 Given("I visit the site") do
-    pending # Write code here that turns the phrase above into concrete actions
+    visit root_path
 end
 
-Given("I click {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+Given("I click {string}") do |link|
+    click_link link
 end
 
 Then("I should be redirected to index page") do
